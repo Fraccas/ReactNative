3e0be31c4b31f4ba50946395d21688456967f0b4
Fraccas <daltonward1@yahoo.com>
2019-09-27 18:04:39 -0500

    UI Updates

--- StarterProject/src/screens/AllBlogs.tsx
+++ StarterProject/src/screens/AllBlogs.tsx
@@ -1,20 +1,57 @@
 import * as React from 'react';
-import { StyleSheet, Text, View, Button } from 'react-native';
-import { NavigationScreenOptions, NavigationScreenProps } from 'react-navigation';
+import { StyleSheet, Text, View, Alert, ScrollView } from 'react-native';
+import { NavigationStackScreenProps } from 'react-navigation-stack';
+import { NavigationScreenOptions } from 'react-navigation';
+import { json } from '../utils/api';
+import BlogPreviewCard from '../components/BlogPreviewCard';
 
-interface IHomeProps extends NavigationScreenProps { }
-interface IHomeState {}
+interface IHomeProps extends NavigationStackScreenProps { }
+interface IHomeState {
+  blogs: {
+    id: number,
+    title: string,
+    content: string,
+    authorid: string,
+    _created: Date
+  }[];
+}
 
 export default class AllBlogs extends React.Component<IHomeProps, IHomeState> {
   static navigationOptions: NavigationScreenOptions = {
     headerTitle: "Blogs"
   };
 
+  constructor(props: IHomeProps) {
+    super(props);
+    this.state = {
+      blogs: []
+    }
+  }
+
+  async componentDidMount() {
+    try {
+      let blogs = await json('https://afternoon-basin-48933.herokuapp.com/api/blogs'); 
+      this.setState({blogs})
+    } catch (e) {
+      console.log(e);
+      Alert.alert("Error on front end api request!");
+    }
+  }
+
+  renderBlogs() {
+    return this.state.blogs.map(blog => {
+      return <BlogPreviewCard key={blog.id} blog={blog}/>
+    });
+  }
+
   render () {
     return (
       <View style={styles.container}>
         <Text style={styles.text}>All Blogs Screen</Text>
-        <Button title="Go to Single Blog" onPress={() => this.props.navigation.navigate('SingleBlog')}/>
+          <ScrollView>
+            {this.renderBlogs()}
+          </ScrollView>
+        
       </View>
     );
   }
